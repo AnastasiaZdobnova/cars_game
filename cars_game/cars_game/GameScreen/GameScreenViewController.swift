21f00760d9aee91f8792c32988d5d153c2b040b6
AnastasiaZdobnova <113519201+AnastasiaZdobnova@users.noreply.github.com>
2024-03-12 00:10:52 +0500

    accident report

--- cars_game/cars_game/GameScreen/GameScreenViewController.swift
+++ cars_game/cars_game/GameScreen/GameScreenViewController.swift
@@ -14,42 +14,165 @@ protocol GameScreenViewControllerProtocol : UIViewController {
 }
 
 class GameScreenViewController: UIViewController, GameScreenViewControllerProtocol {
-    
     var gamePresenter: GameScreenPresenterProtocol
-    
+
+    private let carImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .blue
+        imageView.image = UIImage(named: "car")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+
+    private var incomingCars: [UIImageView] = []
+    private var displayLink: CADisplayLink?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPink // TODO: вынести отдельно
+        view.backgroundColor = .systemPink
         navigationController?.isNavigationBarHidden = false
         drawRoad()
+        setupCar()
+        setupGestureRecognizers()
+        startAddingCars()
+        setupDisplayLink()
     }
     
     func drawRoad() {
-            let roadWidth: CGFloat = 200
-            let laneWidth: CGFloat = roadWidth / 2
-            let dashedLineLength: CGFloat = 10
-            let dashedLineSpacing: CGFloat = 10
+        let roadWidth: CGFloat = 200
+        let laneWidth: CGFloat = roadWidth / 2
+        let dashedLineLength: CGFloat = 10
+        let dashedLineSpacing: CGFloat = 10
+        
+        let roadLayer = CAShapeLayer()
+        roadLayer.frame = CGRect(x: (view.frame.width - roadWidth) / 2, y: 0, width: roadWidth, height: view.frame.height)
+        roadLayer.backgroundColor = UIColor.darkGray.cgColor
+        view.layer.addSublayer(roadLayer)
+        
+        let dashedLineLayer = CAShapeLayer()
+        dashedLineLayer.strokeColor = UIColor.white.cgColor
+        dashedLineLayer.lineWidth = 2
+        dashedLineLayer.lineDashPattern = [NSNumber(value: Float(dashedLineLength)), NSNumber(value: Float(dashedLineSpacing))]
+        let path = CGMutablePath()
+        path.addLines(between: [CGPoint(x: laneWidth, y: -view.frame.height), CGPoint(x: laneWidth, y: 2 * view.frame.height)])
+        dashedLineLayer.path = path
+        roadLayer.addSublayer(dashedLineLayer)
+        
+        let animation = CABasicAnimation(keyPath: "transform.translation.y")
+        animation.fromValue = -view.frame.height
+        animation.toValue = 0
+        animation.duration = 10
+        animation.repeatCount = .infinity
+        dashedLineLayer.add(animation, forKey: "lineAnimation")
+    }
+    
+    private func setupCar() {
+        view.addSubview(carImageView)
+        let roadWidth: CGFloat = 200
+        let laneWidth: CGFloat = roadWidth / 2
+        let carWidth: CGFloat = 50
+        let carHeight: CGFloat = 100
+        carImageView.frame = CGRect(
+            x: (view.frame.width + laneWidth - carWidth) / 2,
+            y: view.frame.height - carHeight - 20,
+            width: carWidth,
+            height: carHeight
+        )
+        
+    }
+    
+    private func setupGestureRecognizers() {
+        let swipeLeftGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe(_:)))
+        swipeLeftGestureRecognizer.direction = .left
+        view.addGestureRecognizer(swipeLeftGestureRecognizer)
+        
+        let swipeRightGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe(_:)))
+        swipeRightGestureRecognizer.direction = .right
+        view.addGestureRecognizer(swipeRightGestureRecognizer)
+    }
+    
+    @objc private func handleSwipe(_ gesture: UISwipeGestureRecognizer) {
+        let roadWidth: CGFloat = 200
+        let laneWidth: CGFloat = roadWidth / 2
+        let carWidth: CGFloat = 50
+        
+        switch gesture.direction {
+        case .left:
+            if carImageView.frame.minX > (view.frame.width - roadWidth) / 2 {
+                UIView.animate(withDuration: 0.3) {
+                    self.carImageView.frame.origin.x -= laneWidth
+                }
+            }
+        case .right:
+            if carImageView.frame.maxX < (view.frame.width + roadWidth) / 2 {
+                UIView.animate(withDuration: 0.3) {
+                    self.carImageView.frame.origin.x += laneWidth
+                }
+            }
+        default:
+            break
+        }
+    }
+    
+    private func addIncomingCar() {
+        let incomingCarImageView = UIImageView(image: UIImage(named: "megaCar"))
+        incomingCarImageView.contentMode = .scaleAspectFit
+        incomingCarImageView.backgroundColor = .yellow
 
-            let roadLayer = CAShapeLayer()
-            roadLayer.frame = CGRect(x: (view.frame.width - roadWidth) / 2, y: 0, width: roadWidth, height: view.frame.height)
-            roadLayer.backgroundColor = UIColor.darkGray.cgColor
-            view.layer.addSublayer(roadLayer)
+        let roadWidth: CGFloat = 200
+        let laneWidth: CGFloat = roadWidth / 2
+        let carWidth: CGFloat = 50
+        let carHeight: CGFloat = 100
+        
 
-            let dashedLineLayer = CAShapeLayer()
-            dashedLineLayer.strokeColor = UIColor.white.cgColor
-            dashedLineLayer.lineWidth = 2
-            dashedLineLayer.lineDashPattern = [NSNumber(value: Float(dashedLineLength)), NSNumber(value: Float(dashedLineSpacing))]
-            let path = CGMutablePath()
-            path.addLines(between: [CGPoint(x: laneWidth, y: -view.frame.height), CGPoint(x: laneWidth, y: 2 * view.frame.height)])
-            dashedLineLayer.path = path
-            roadLayer.addSublayer(dashedLineLayer)
+        let randomLane = Int.random(in: 0...1)
+        let xPos = (view.frame.width - roadWidth) / 2 + (randomLane == 0 ? 0 : laneWidth)
 
-            let animation = CABasicAnimation(keyPath: "transform.translation.y")
-            animation.fromValue = -view.frame.height
-            animation.toValue = 0
-            animation.duration = 10
-            animation.repeatCount = .infinity
-            dashedLineLayer.add(animation, forKey: "lineAnimation")
+        incomingCarImageView.frame = CGRect(
+            x: xPos,
+            y: -carHeight, // Start above the screen
+            width: carWidth,
+            height: carHeight
+        )
+
+        view.addSubview(incomingCarImageView)
+        incomingCars.append(incomingCarImageView)
+
+        UIView.animate(withDuration: 10, delay: 0, options: [.curveLinear], animations: {
+            incomingCarImageView.frame.origin.y = self.view.frame.height
+        }) { [weak self] _ in
+            self?.incomingCars.removeAll { $0 === incomingCarImageView }
+            incomingCarImageView.removeFromSuperview()
+        }
+    }
+    
+    private func startAddingCars() {
+            Timer.scheduledTimer(withTimeInterval: 3, repeats: true) { [weak self] timer in
+                self?.addIncomingCar()
+            }
+        }
+    
+    private func setupDisplayLink() {
+            displayLink = CADisplayLink(target: self, selector: #selector(step))
+            displayLink?.add(to: .main, forMode: .default)
+        }
+
+    @objc private func step(displaylink: CADisplayLink) {
+        for incomingCar in incomingCars {
+            if let presentationFrame = incomingCar.layer.presentation()?.frame {
+                if carImageView.frame.intersects(presentationFrame) {
+                    handleCollision()
+                    return
+                }
+            }
+        }
+    }
+    private func handleCollision() {
+            print("Столкновение произошло!")
+            displayLink?.invalidate()
+            displayLink = nil
+            
+            // Останавливаем игру или показываем алерт
         }
     
     init(presenter: GameScreenPresenterProtocol) {
@@ -60,4 +183,7 @@ class GameScreenViewController: UIViewController, GameScreenViewControllerProtoc
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    deinit {
+           displayLink?.invalidate()
+       }
 }
